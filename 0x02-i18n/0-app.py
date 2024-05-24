7ce42f45ccff9d8ff473ff4f455dba3629b819c0
NgigiN <ngigi.nyongo@gmail.com>
2024-05-24 12:40:11 +0300

    Basic Flask App

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,7 +10,7 @@ app.url_map.stric_slashes = False
 @app.route('/')
 def index() -> str:
   """The home/index page"""
-  return render_template('index.html')
+  return render_template("0-index.html",)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
