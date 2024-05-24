f2f9ceb44da9575344fb9283f6cb2e277da99875
NgigiN <ngigi.nyongo@gmail.com>
2024-05-24 09:07:16 +0300

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -33,6 +33,8 @@ class Server:
             return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrives a page of data
+        """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start_index, end_index = index_range(page, page_size)
