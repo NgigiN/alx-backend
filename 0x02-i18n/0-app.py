fdcb2e36a0f4727860fc4231db474474da9157b9
NgigiN <ngigi.nyongo@gmail.com>
2024-05-24 12:02:08 +0300

    Basic Flask App

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,13 @@
-from app import app
+#!/usr/bin/env python3
+"""A basic app"""
+from flask import Flask, render_template
+app = Flask(__name__)
+app.url_map.stric_slashes = False
+
+@app.route('/')
+def get_index() -> str:
+  """The home/index page"""
+  return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
