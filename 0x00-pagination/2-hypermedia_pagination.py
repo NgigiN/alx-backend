d710473a8954534d17e09dc18c38663093fb0a0d
NgigiN <ngigi.nyongo@gmail.com>
2024-05-24 09:35:24 +0300

    hyper pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -35,8 +35,9 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Retrives a page of data
         """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
+        assert isinstance(page, int) and page > 0, "Page must be int > than 0."
+        assert isinstance(page_size, int) and page_size > 0, "be int > than 0."
+
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
 
